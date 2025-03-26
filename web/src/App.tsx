f71c78f883d47f49714d116d504840f1ea3f4cef
natewong1313 <natewong1@gmail.com>
2025-03-26 12:50:54 -0400

    annoying ah frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <main className="bg-black min-h-screen flex flex-col">
+      <nav className="flex justify-between items-center border-b border-neutral-600 py-4 px-8 text-white">
+        <h1>simple video transcriber</h1>
+        <a
+          href="https://www.github.com/natewong1313/video-transcriber"
+          target="_black"
+          className="font-semibold"
+        >
+          source code
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </nav>
+    </main>
+  );
 }
 
-export default App
+export default App;
