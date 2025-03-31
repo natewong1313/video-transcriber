0baca859417cb961c781806ff2dcb65a3d2d4838
natewong1313 <natewong1@gmail.com>
2025-03-31 11:17:25 -0400

    add tanstack router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,22 @@
+import { Clapperboard } from "lucide-react";
+
 function App() {
   return (
-    <main className="bg-black min-h-screen flex flex-col">
-      <nav className="flex justify-between items-center border-b border-neutral-600 py-4 px-8 text-white">
-        <h1>simple video transcriber</h1>
+    <main className="min-h-screen flex flex-col bg-gray-100">
+      <nav className="flex justify-between items-center py-6 bg-white px-8 border-b border-gray-200">
+        <div className="flex items-center justify-center space-x-2">
+          <Clapperboard
+            className="bg-emerald-500 p-2 text-white rounded-md"
+            size={40}
+          />
+          <h1>nate's video transcriber</h1>
+        </div>
         <a
           href="https://www.github.com/natewong1313/video-transcriber"
-          target="_black"
-          className="font-semibold"
+          target="_blank"
+          className="hover:underline"
         >
-          source code
+          please login
         </a>
       </nav>
     </main>
