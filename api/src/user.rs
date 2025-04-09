275dcff690168cdd5e52ef829e0e5aa01e22bdba
natewong1313 <natewong1@gmail.com>
2025-04-09 14:00:58 -0400

    refactor create user function

--- api/src/user.rs
+++ api/src/user.rs
@@ -31,12 +31,18 @@ pub fn user_router(state: AppState) -> Router {
     Router::new().nest("/users", router).with_state(state)
 }
 
+const UNKNOWN_ERR: (StatusCode, &str) =
+    (StatusCode::INTERNAL_SERVER_ERROR, "Unknown error occured");
+
 async fn create(State(state): State<AppState>, Json(payload): Json<CreateUser>) -> Response {
     let argon2 = Argon2::default();
     let salt = SaltString::generate(&mut OsRng);
     let password_hash = match argon2.hash_password(payload.password.as_bytes(), &salt) {
         Ok(hash) => hash,
-        Err(err) => return (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response(),
+        Err(err) => {
+            println!("Error occured hashing password: {}", err);
+            return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to hash password").into_response();
+        }
     };
 
     let user = User {
@@ -45,25 +51,27 @@ async fn create(State(state): State<AppState>, Json(payload): Json<CreateUser>) 
         password_hash: password_hash.to_string(),
     };
 
-    let user_id = user.id.to_string();
-
-    match sqlx::query!(
-        r#"
-    INSERT INTO users (id, email, password_hash)
-    VALUES ( ?1, ?2, ?3 )
-    "#,
-        user_id,
-        user.email,
-        user.password_hash
-    )
-    .execute(&state.pool)
-    .await
+    if let Err(err) =
+        sqlx::query("INSERT INTO USERS (id, email, password_hash) VALUES ($1, $2, $3)")
+            .bind(user.id.to_string())
+            .bind(&user.email)
+            .bind(&user.password_hash)
+            .execute(&state.pool)
+            .await
     {
-        Ok(res) => println!("{}", res.rows_affected()),
-        Err(err) => {
-            // let err = err.into_database_error();
-            return (StatusCode::BAD_REQUEST, "User with email already exists").into_response();
-        }
+        println!("error occured creating user: {}", err);
+        let err_code = match err.as_database_error() {
+            Some(err) => err.code(),
+            None => return UNKNOWN_ERR.into_response(),
+        };
+        let response = match err_code {
+            Some(code) if code == "2067" => {
+                (StatusCode::BAD_REQUEST, "User with email already exists")
+            }
+            Some(_) => UNKNOWN_ERR,
+            None => UNKNOWN_ERR,
+        };
+        return response.into_response();
     };
 
     (StatusCode::CREATED, Json(user)).into_response()
