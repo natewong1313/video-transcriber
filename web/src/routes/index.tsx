145eeafa9214a025924213d4774cfc94870c3cb0
natewong1313 <natewong1@gmail.com>
2025-04-29 13:11:03 -0400

    fix frontend file upload, remove dep

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { Clapperboard } from "lucide-react";
 import { getAuthenticatedUser } from "../lib/auth";
-import { useFilePicker } from "use-file-picker";
-import { FileContent } from "use-file-picker/types";
+import { useMutation } from "@tanstack/react-query";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
@@ -13,16 +13,47 @@ function RouteComponent() {
   const user = Route.useLoaderData();
   const navigate = useNavigate();
 
-  const { openFilePicker, filesContent, loading } = useFilePicker({
-    accept: ".mp4",
-  });
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-  const onOpenFilePicker = () => {
+  const openFilePicker = () => {
     if (user) {
-      openFilePicker();
+      fileInputRef.current?.click();
     } else {
       navigate({ to: "/login" });
     }
+  };
+  const onFileSelect = (e: ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files || files.length < 0) {
+      return;
+    }
+    setSelectedFile(files[0]);
+  };
+
+  const transcribeMutation = useMutation({
+    mutationFn: async (file: File) => {
+      const formData = new FormData();
+      formData.append("file", file);
+      const response = await fetch("/api/transcribe/new", {
+        method: "POST",
+        body: formData,
+      });
+      if (!response.ok) {
+        console.log(response);
+        // const message = (await response.json())["message"];
+        // throw new Error(message);
+      }
+    },
+    // onSuccess: async () => {
+    //   navigate({ to: "/" });
+    // },
+  });
+
+  const onTranscribeSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    selectedFile && (await transcribeMutation.mutateAsync(selectedFile));
   };
   return (
     <main className="min-h-screen flex flex-col bg-gray-100">
@@ -44,33 +75,39 @@ function RouteComponent() {
           </Link>
         )}
       </nav>
-      <div className="flex py-16 items-center flex-col">
+      <form
+        className="flex py-16 items-center flex-col"
+        onSubmit={(e) => onTranscribeSubmit(e)}
+      >
         <h1 className="text-5xl font-bold">transcribe video to text</h1>
         <p className="mt-2">
           simply upload your video below and get the full transcript
         </p>
-        {filesContent.length == 0 ? (
-          <UploadFileComponent
-            openFilePicker={onOpenFilePicker}
-            loading={loading}
-          />
+        <input
+          type="file"
+          accept=".mp4"
+          className="hidden"
+          ref={fileInputRef}
+          onChange={onFileSelect}
+        />
+        {!selectedFile ? (
+          <UploadFileComponent openFilePicker={openFilePicker} />
         ) : (
           <ViewFileComponent
-            openFilePicker={onOpenFilePicker}
-            filesContent={filesContent}
+            openFilePicker={openFilePicker}
+            selectedFile={selectedFile}
+            loading={transcribeMutation.isPending}
           />
         )}
-      </div>
+      </form>
     </main>
   );
 }
 
 function UploadFileComponent({
   openFilePicker,
-  loading,
 }: {
   openFilePicker: () => void;
-  loading: boolean;
 }) {
   return (
     <div className="mt-8 bg-white h-56 w-[30rem] rounded-md flex items-center justify-center flex-col">
@@ -79,11 +116,11 @@ function UploadFileComponent({
         click the choose file button or drag and drop files below
       </p>
       <button
+        type="button"
         onClick={openFilePicker}
         className="bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 text-white rounded-md py-2 px-4 mt-4"
-        disabled={loading}
       >
-        {loading ? "..." : "choose file"}
+        choose file
       </button>
     </div>
   );
@@ -91,30 +128,31 @@ function UploadFileComponent({
 
 function ViewFileComponent({
   openFilePicker,
-  filesContent,
+  selectedFile,
+  loading,
 }: {
   openFilePicker: () => void;
-  filesContent: FileContent<string>[];
+  selectedFile: File;
+  loading: boolean;
 }) {
-  const loading = false;
   return (
     <div className="mt-8 bg-white h-56 w-[30rem] p-6 rounded-md flex items-center justify-center flex-col">
       <h2 className="text-lg font-medium">all set!</h2>
       <p className="my-2">
         review your file below and press transcribe when ready
       </p>
-      {filesContent.map((file) => (
-        <div className="text-xs w-full bg-gray-100 rounded-md px-2 py-4 flex items-center">
-          <p className="truncate mr-2">{file.name}</p>
-          <button
-            onClick={openFilePicker}
-            className="ml-auto rounded-md py-2 px-3 ring ring-gray-400 hover:bg-gray-200"
-          >
-            change
-          </button>
-        </div>
-      ))}
+      <div className="text-xs w-full bg-gray-100 rounded-md px-2 py-4 flex items-center">
+        <p className="truncate mr-2">{selectedFile.name}</p>
+        <button
+          type="button"
+          onClick={openFilePicker}
+          className="ml-auto rounded-md py-2 px-3 ring ring-gray-400 hover:bg-gray-200"
+        >
+          change
+        </button>
+      </div>
       <button
+        type="submit"
         className="bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 text-white rounded-md py-2 px-4 mt-4"
         disabled={loading}
       >
